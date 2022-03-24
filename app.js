7b3c19166ff167e38e7b16d1913fb28ec7b083d8
ZahraO1 <zahra.masroor6@gmail.com>
2022-03-23 21:05:31 -0400

    created a Casino game using JavaScript

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.timesPlayed = 0;
+        this.name = name;
+        this.isFakeCoin = isFakeCoin;
+    }
+    playGame(betAmount){
+        this.timesPlayed ++;
+        if(Math.random() <= 0.5 || this.isFakeCoin == true)
+        {
+            console.log(`${this.name} wins`)
+        }
+        else{
+            console.log(`Player wins ${betAmount * (this.timesPlayed + 1)}`)
+        }
+    }
+    rollDie(d){
+        console.log(parseInt(Math.random()*d)+1)
+    }
 };
 
 // TESTS
@@ -11,20 +28,19 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
